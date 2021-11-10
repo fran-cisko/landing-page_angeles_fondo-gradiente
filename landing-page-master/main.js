33bb75e9219289f0f0e543e1fc7a7aa1953b3b07
fran-cisko <franciscosciarretta@gmail.com>
2021-11-10 13:03:35 -0300

    actualización del index en root para publicarla

--- landing-page-master/main.js
+++ landing-page-master/main.js
@@ -1,5 +1,5 @@
 const init = async () => {
-	const config = await (await fetch('config.json')).json();
+	const config = await (await fetch('landing-page-master/config.json')).json();
 	let newHTML = document.documentElement.innerHTML;
 	Object.keys(config).map((key) => {
 		newHTML = newHTML.replace(key, config[key]);
